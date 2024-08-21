9aef8378269f7446aa5281ced2abdd6557782b40
RichardSchonfeld <richardschonfeld@yahoo.com>
2024-08-21 18:00:22 +0200

    Some cleanup + Signing certs with Django users now works

--- VerifyDjango/Verify_app/views.py
+++ VerifyDjango/Verify_app/views.py
@@ -1,34 +1,25 @@
 import hashlib
 import json
 
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from eth_account.messages import encode_defunct
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 
 import requests
 import ast
 from web3 import Web3
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 
 from .models import Claim, CustomUser, Certificate
 from .forms import UserRegisterForm
 from .ipfs_functions import get_ipfs_raw_data, parse_json_decrypted_ipfs_data, get_ipfs_decrypted_data
-
-from .serializers import ClaimSerializer
 from .eth_utils import create_claim, sign_claim, get_claim, fund_account
-
 from .encryption_utils import encrypt_private_key, derive_key, decrypt_private_key, encrypt_and_split, decrypt_with_shares
-
-from .exceptions import IPFSHashNotReturnedException
 
 
 def index(request):
@@ -55,21 +46,6 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
-
-@login_required
-def recover_key(request):
-    if request.method == 'POST':
-        password = request.POST.get('password')
-        user = request.user
-
-        try:
-            private_key = decrypt_private_key(user.encrypted_private_key, password)
-            return render(request, 'recover_key.html', {'private_key': private_key})
-        except Exception as e:
-            return render(request, 'recover_key.html',
-                          {'error': 'Invalid password or key could not be decrypted.'})
-
-    return render(request, 'recover_key.html')
 
 def transaction_confirmation(request):
     txn_hash = request.GET.get('txn_hash')
@@ -147,112 +123,6 @@ def create_claim(request):
         authorities = CustomUser.objects.filter(is_authority=True)
         return render(request, 'create_claim.html', {'authorities': authorities})
 
-@login_required
-def create_claim_dep(request):
-    if request.method == 'POST':
-        year_of_graduation = request.POST['year_of_graduation']
-        student_number = request.POST['student_number']
-        full_name = request.POST['full_name']
-        authority_public_key = request.POST['authority']
-
-        import json
-        # Load contract ABI and address
-        with open('build/contracts/Verify.json') as f:
-            contract_data = json.load(f)
-            contract_abi = contract_data['abi']
-        # Update the contract address here after migration
-        contract_address = settings.CONTRACT_ADDRESS
-        # Connect to local Ganache
-        web3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
-        # Initialize the contract
-        verify_contract_instance = web3.eth.contract(address=contract_address, abi=contract_abi)
-
-        # Encrypting data and getting key shares
-        data = f"{year_of_graduation},{student_number},{full_name}"
-        encrypted_data, shares = encrypt_and_split(data)
-
-        print("SHARES")
-        print(shares)
-
-        # Uploading to IPFS and getting hash back
-        from .web3_utils import upload_to_ipfs
-        IPFS_hash = upload_to_ipfs(encrypted_data)
-
-        print("IPFS_hash")
-        print(IPFS_hash)
-
-        user_profile = request.user
-
-        # Save shamir fragment and IPFS hash to user profile
-        from .forms import store_key_fragment
-        store_key_fragment(user_profile, shares, IPFS_hash)
-
-        # Determine the type of user
-        #if isinstance(request.user, Web3Account):
-        if user_profile.is_web3_user:
-            # MetaMask user - Web3Account
-            wallet_address = Web3.to_checksum_address(user_profile.public_key)
-
-            # Prepare the transaction data for MetaMask
-            transaction = verify_contract_instance.functions.createClaim(
-                _requester=wallet_address,
-                _authority=authority_public_key,
-                _yearOfGraduation=year_of_graduation,
-                _studentNumber=student_number,
-                _fullName=full_name,
-                _ipfsHash=IPFS_hash
-            )
-            context = {
-                'transaction_data': transaction.build_transaction({
-                    'chainId': 1337,  # Ganache
-                    'gas': 300000,
-                    'gasPrice': web3.to_wei('50', 'gwei'),
-                    'nonce': web3.eth.get_transaction_count(wallet_address),
-                    'from': wallet_address
-                }),
-                'use_metamask': True,
-            }
-            return render(request, 'submit_metamask.html', context)
-
-        elif not user_profile.is_web3_user:
-
-            wallet_address = Web3.to_checksum_address(user_profile.public_key)
-            from .encryption_utils import decrypt_private_key
-
-            private_key = decrypt_private_key(user_profile.encrypted_private_key, request.POST['password'])
-            # Prepare the transaction data for Django user
-
-            transaction = verify_contract_instance.functions.createClaim(
-                _requester=wallet_address,
-                _authority=authority_public_key,
-                _yearOfGraduation=year_of_graduation,
-                _studentNumber=student_number,
-                _fullName=full_name,
-                _ipfsHash=IPFS_hash
-            )
-
-            built_txn = transaction.build_transaction({
-                'chainId': 1337,  # Ganache
-                'gas': 300000,
-                'gasPrice': web3.to_wei('50', 'gwei'),
-                'nonce': web3.eth.get_transaction_count(wallet_address),
-            })
-
-            # Sign the transaction
-            signed_txn = web3.eth.account.sign_transaction(built_txn, private_key)
-            # Send the transaction
-            txn_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-
-            context = {
-                'txn_hash': txn_hash.hex(),
-                'use_metamask': False,
-            }
-
-            return render(request, 'submit_confirmation.html', context)
-    else:
-        authorities = CustomUser.objects.filter(is_authority=True)
-        return render(request, 'create_claim.html', {'authorities': authorities})
-
 def view_claims(request):
     claims = get_claim()
     return render(request, 'view_claims.html', {'claims': claims})
@@ -260,6 +130,7 @@ def view_claims(request):
 
 #### DOUBLE CHECK IF CSRF EXEMPT IS OKAY HERE ####
 @csrf_exempt
+@login_required
 def sign_certificate(request):
     """Handle certificate signing and storage."""
     if request.method == 'POST':
@@ -292,7 +163,17 @@ def sign_certificate(request):
             return JsonResponse({"error": str(e)}, status=500)
 
     # Render the signing page for GET requests
-    return render(request, 'sign_certificate.html')
+    user = request.user
+    context = {
+        'is_web3_user': True
+    }
+
+    if not user.is_web3_user:
+        context = {
+            'encrypted_private_key': request.user.encrypted_private_key,
+            'is_web3_user': False
+        }
+    return render(request, 'sign_certificate.html', context)
 
 
 @csrf_exempt
@@ -358,26 +239,6 @@ def verify_signature(request):
     # Render the form page for GET requests
     return render(request, 'verify_signature.html')
 
-class SignClaimView(APIView):
-    def post(self, request):
-        data = request.data
-        claim_id = data.get('claim_id')
-        authority_address = data.get('authority_address')
-
-        tx_hash = sign_claim(claim_id, authority_address)
-
-        claim = Claim.objects.get(id=claim_id)
-        claim.signed = True
-        claim.transaction_hash = tx_hash
-        claim.save()
-
-        return Response({"tx_hash": tx_hash}, status=status.HTTP_200_OK)
-
-
-from django.shortcuts import render
-from django.http import JsonResponse
-
-
 def decrypt_claim(request):
     if request.method == 'POST':
         ipfs_hash = request.POST['ipfs_hash']
@@ -406,6 +267,20 @@ def decrypt_claim(request):
     return render(request, 'decrypt_claim.html')
 
 
+@login_required
+def recover_key(request):
+    if request.method == 'POST':
+        password = request.POST.get('password')
+        user = request.user
+
+        try:
+            private_key = decrypt_private_key(user.encrypted_private_key, password)
+            return render(request, 'recover_key.html', {'private_key': private_key})
+        except Exception as e:
+            return render(request, 'recover_key.html',
+                          {'error': 'Invalid password or key could not be decrypted.'})
+
+    return render(request, 'recover_key.html')
 
 
 
@@ -413,6 +288,3 @@ class ListClaimsView(APIView):
     def get(self, request):
         claim = get_claim()
         return Response(claim)
-
-    #queryset = Claim.objects.all()
-    #serializer_class = ClaimSerializer
