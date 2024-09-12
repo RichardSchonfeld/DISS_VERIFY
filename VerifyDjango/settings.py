64352e002f6706b8ac2577f36e48d5333f9ec37b
Richard Schonfeld <richardschonfeld@yahoo.com>
2024-09-12 17:14:25 +0200

    adjustment of allowed hosts

--- VerifyDjango/settings.py
+++ VerifyDjango/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-#your_secret_key_here'
 DEBUG = True
-ALLOWED_HOSTS = ['https://verify-119c36994002.herokuapp.com/']
+ALLOWED_HOSTS = ['verify-119c36994002.herokuapp.com/']
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
