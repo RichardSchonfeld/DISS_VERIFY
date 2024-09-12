aec8cfe1c025697918fdfe3465b28629480e8372
RichardSchonfeld <richardschonfeld@yahoo.com>
2024-09-12 13:15:17 +0200

    Databses config update

--- VerifyDjango/settings.py
+++ VerifyDjango/settings.py
@@ -58,14 +58,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'VerifyDjango.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-django_heroku.settings(locals())
 
 AUTH_USER_MODEL = 'Verify_app.CustomUser'
 AUTH_PASSWORD_VALIDATORS = [
